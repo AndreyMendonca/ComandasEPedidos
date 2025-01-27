5d63748e393de21de0a55f39c3546ff199a48f22
Andrey Mendonca <andrey.mmendonca@gmail.com>
2025-01-27 14:34:23 -0300

    Projeto criado

---
+++ src/main/java/com/comandaspedidos/ComandaEPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.comandaspedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComandaEPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComandaEPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comandaspedidos/ComandaEPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comandaspedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComandaEPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
