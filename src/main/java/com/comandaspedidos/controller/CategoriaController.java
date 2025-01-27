015045232ddcdde9e32836dc7d7d8d19ed85928b
Andrey Mendonca <andrey.mmendonca@gmail.com>
2025-01-27 17:47:44 -0300

    Liberar o cross

--- src/main/java/com/comandaspedidos/controller/CategoriaController.java
+++ src/main/java/com/comandaspedidos/controller/CategoriaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.comandaspedidos.service.CategoriaService;
 
 @RestController
 @RequestMapping("/categorias")
+@CrossOrigin(origins = "**")
 public class CategoriaController {
 	@Autowired
 	private CategoriaService service;
